92c01bb842a83bc4590137b2d6c46621c530268e
Jfreijser <freijserjaron@gmail.com>
2026-04-20 19:39:33 +0200

    Add project files.

---
+++ ActivityAssistent.Shared/Models/ActionPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityAssistent.Shared.Models
+{
+    public class ActionPoint
+    {
+        public Guid ActionPointId { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+        public DateTime? DueDate { get; set; }
+
+        // De koppeling naar het gesprek
+        public Guid SalesConversationId { get; set; }
+    }
+}
---
+++ ActivityAssistent.Shared/Models/SalesConversation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityAssistent.Shared.Models
+{
+    public class SalesConversation
+    {
+        public Guid SalesConversationId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime MeetingDate { get; set; } = DateTime.Now;
+        public string SalesPersonId { get; set; } = string.Empty;
+        public string? RecordingBlobUri { get; set; }
+        public string? AiSummary { get; set; }
+    }
+}
