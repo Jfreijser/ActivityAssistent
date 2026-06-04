55529d74a6e4b98f1b38475f42224f99e9ceeac1
Jfreijser <freijserjaron@gmail.com>
2026-06-04 18:23:08 +0200

    fix na Ios migratie

--- ActivityAssistent.App/MainPage.xaml.cs
+++ ActivityAssistent.App/MainPage.xaml.cs
@@ -1,10 +1,20 @@
-﻿namespace ActivityAssistent.App
+﻿using ActivityAssistent.App.Components;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace ActivityAssistent.App
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            blazorWebView.RootComponents.Add(
+                new RootComponent
+                {
+                    Selector = "#app",
+                    ComponentType = typeof(Routes)
+                });
         }
     }
 }
