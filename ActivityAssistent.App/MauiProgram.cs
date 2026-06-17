4ef34878fd88509c557432232a7c87d959b23cb8
Jfreijser <freijserjaron@gmail.com>
2026-06-17 09:13:14 +0200

    android emulator opstart mogelijkheid toegevoegd

--- ActivityAssistent.App/MauiProgram.cs
+++ ActivityAssistent.App/MauiProgram.cs
@@ -39,25 +39,43 @@ namespace ActivityAssistent.App
             builder.Services.AddRadzenComponents();
             Action<HttpClient> ConfigureBackendClient = Client =>
             {
-                // Vergeet niet dat localhost voor een Android emulator straks 10.0.2.2 wordt
+                // Gebruik 10.0.2.2 voor de Android emulator, anders localhost
+#if ANDROID
+                Client.BaseAddress = new Uri("https://10.0.2.2:7230");
+#else
                 Client.BaseAddress = new Uri("https://localhost:7230");
+#endif
                 Client.Timeout = TimeSpan.FromMinutes(5);
             };
 
 #if DEBUG
             builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
-            builder.Services.AddHttpClient<IConversationService, MauiConversationService>(ConfigureBackendClient);
-            builder.Services.AddHttpClient<IActionPointService, MauiActionPointService>(ConfigureBackendClient);
-            builder.Services.AddHttpClient<ICompanyService, MauiCompanyService>(ConfigureBackendClient);
-            builder.Services.AddHttpClient<IAuthService, MauiAuthService>(ConfigureBackendClient);
-            builder.Services.AddHttpClient<IAudioRecorderService, MauiAudioService>(ConfigureBackendClient);
-            builder.Services.AddHttpClient<IAiMeetingAnalyzer, MauiAiService>(ConfigureBackendClient);
-            builder.Services.AddHttpClient<IAgenda, MauiAgendaService>(ConfigureBackendClient);
+            AddActivityAssistentHttpClient<IConversationService, MauiConversationService>(builder.Services, ConfigureBackendClient);
+            AddActivityAssistentHttpClient<IActionPointService, MauiActionPointService>(builder.Services, ConfigureBackendClient);
+            AddActivityAssistentHttpClient<ICompanyService, MauiCompanyService>(builder.Services, ConfigureBackendClient);
+            AddActivityAssistentHttpClient<IAuthService, MauiAuthService>(builder.Services, ConfigureBackendClient);
+            AddActivityAssistentHttpClient<IAudioRecorderService, MauiAudioService>(builder.Services, ConfigureBackendClient);
+            AddActivityAssistentHttpClient<IAiMeetingAnalyzer, MauiAiService>(builder.Services, ConfigureBackendClient);
+            AddActivityAssistentHttpClient<IAgenda, MauiAgendaService>(builder.Services, ConfigureBackendClient);
             builder.Services.AddScoped<IThemeService, Services.ThemeService>();
             builder.Services.AddScoped<IEmailService, MauiEmailService>();
             return builder.Build();
+        }
+
+        private static IHttpClientBuilder AddActivityAssistentHttpClient<TClient, TImplementation>(IServiceCollection services, Action<HttpClient> configureClient)
+            where TClient : class
+            where TImplementation : class, TClient
+        {
+            var builder = services.AddHttpClient<TClient, TImplementation>(configureClient);
+#if ANDROID
+            builder.ConfigurePrimaryHttpMessageHandler(() => new Xamarin.Android.Net.AndroidMessageHandler
+            {
+                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+            });
+#endif
+            return builder;
         }
     }
 }
